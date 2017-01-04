d983223d718f7857179035001ee19c97d91afa78
davidmgudeman <davidmgudeman@gmail.com>
2017-01-04 10:15:13 -0800

    Interfaces

--- d_oneWayBind/app/books/books-list/books-list.component.ts
+++ d_oneWayBind/app/books/books-list/books-list.component.ts
@@ -1,4 +1,16 @@
 import { Component } from '@angular/core';
+interface IBook{
+    bookAuthor: string;
+    bookTitle: string;
+    bookPrice: number;
+    bookDescription:  string;
+    publishedOn: Date;
+    inStock: string;
+    bookReviews: number;
+    bookImageUrl: string;
+    hardcover?: boolean;
+
+}
 
 @Component({
     moduleId: module.id,
@@ -11,7 +23,7 @@ export class BooksListComponent {
     showImage:boolean = true;
     booksInStock:number = 2;
 
-    books:any[] = [{
+    books:IBook[] = [{
         bookAuthor: "Tom Jones",
         bookTitle: "War and Peace 2",
         bookPrice: 29.95,
@@ -19,7 +31,8 @@ export class BooksListComponent {
         publishedOn: new Date('02/11/1921'),
         inStock:"yes",
         bookReviews: 15,
-        bookImageUrl: "app/assets/images/656.jpg"
+        bookImageUrl: "app/assets/images/656.jpg",
+        hardcover:false
     }]
 
     toggleImage():void {
