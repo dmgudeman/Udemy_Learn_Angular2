cb265aa8c642014470523e9db02c980ee2ab45aa
davidmgudeman <davidmgudeman@gmail.com>
2017-01-03 18:18:32 -0800

    Custom directives

--- d_oneWayBind/app/app.module.ts
+++ d_oneWayBind/app/app.module.ts
@@ -3,11 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent }   from './app.component';
 import { BooksListComponent }   from './books/books-list/books-list.component';
+import { HighlightDirective } from './shared/highlightt.directive';
 
 @NgModule({
     imports: [BrowserModule, FormsModule],
     declarations: [AppComponent,
-        BooksListComponent],
+        BooksListComponent,
+        HighlightDirective
+    ],
     bootstrap: [AppComponent]
 })
 
