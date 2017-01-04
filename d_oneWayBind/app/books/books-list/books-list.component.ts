3f3e451fcf3dca700bd3e421336b086dfb5ed843
davidmgudeman <davidmgudeman@gmail.com>
2017-01-04 11:46:19 -0800

    ngInit and onChanges

--- d_oneWayBind/app/books/books-list/books-list.component.ts
+++ d_oneWayBind/app/books/books-list/books-list.component.ts
@@ -1,31 +1,31 @@
-import { Component } from '@angular/core';
-import { IBook } from '../books.book';
-/**
-interface IBook{
-    bookAuthor: string;
-    bookTitle: string;
-    bookPrice: number;
-    bookDescription:  string;
-    publishedOn: Date;
-    inStock: string;
-    bookReviews: number;
-    bookImageUrl: string;
-    hardcover?: boolean;
+import { Component, OnInit, OnChanges } from '@angular/core';
 
-}
-*/
 @Component({
     moduleId: module.id,
     selector: 'bs-books-list',
     templateUrl: 'books-list.component.html'
 })
 
-export class BooksListComponent {
+export class BooksListComponent implements OnInit, OnChanges {
+
+    ngOnInit() {
+        console.log('Init', this.booksInStock);
+    }
+
+    ngOnChanges() {
+        console.log('new change detected');
+    }
+
+    changeMethod():void {
+        this.animals=['dog', 'cat'];
+        console.log('change method happened', this.animals);
+    }
+    animals:string[]=['zebra','moose'];
     imageWidth:number = 100;
     showImage:boolean = true;
     booksInStock:number = 2;
 
-    books:IBook[] = [{
+    books:any[] = [{
         bookAuthor: "Tom Jones",
         bookTitle: "War and Peace 2",
         bookPrice: 29.95,
